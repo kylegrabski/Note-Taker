c57f0d113fe1c884567cecc2971d54e072aaecdc
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-24 15:00:58 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path")
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Middleware Functions
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// to get styles
+// tells the server that we allow access to our public file, that way it
+// will have access to our css pages
+app.use(express.static("public"));
+
+
+// app.get("/", function(req, res){
+//     req.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+
+// HTML ROUTES
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+    // Retrieve all notes and the res.json them back to the front end
+});
+
+// API ROUTES
+app.get("/api/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+    // Retrieve all notes and the res.json them back to the front end
+});
+
+app.post("/api/notes", function(req, res){
+    // creates a note from req.body
+  
+});
+
+app.delete("api/notes/:id", function(req, res){
+    // Delete a note based off id
+    const { id } = req.params;
+
+});
+
+
+app.listen(PORT, () => console.log("App listening on port " + PORT));
